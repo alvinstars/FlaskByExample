62a9b721fad71cef87d0b5b1cf889425271f5b42
Bin Yang <alvinstars@gmail.com>
2017-07-09 01:29:24 -0400

    ninja tempaltes

--- Project2/headlines/headlines.py
+++ Project2/headlines/headlines.py
@@ -13,12 +13,9 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
     return render_template("home.html",
                            publication=publication.upper(),
-                           title=first_article.get("title"),
-                           published=first_article.get("published"),
-                           summary=first_article.get("summary")
+                           articles=feed['entries']
                            )
 
 
